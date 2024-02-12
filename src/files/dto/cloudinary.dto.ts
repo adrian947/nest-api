e5e0823651d65323b775dbc8c4956aecabbc0bd0
adrian <aadducchio@leverbox.com>
2024-02-12 13:35:57 -0300

    add documentation dto files

--- src/files/dto/cloudinary.dto.ts
+++ src/files/dto/cloudinary.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsBoolean, IsDefined, IsOptional, IsString, } from "class-validator";
 
 export class CloudinaryDTO {
+    @ApiProperty()
     @IsDefined()
     @IsOptional()
     @IsBoolean()        
